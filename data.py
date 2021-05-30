ff1f9370faa0069184d39d103a708c58c73a3338
Kai-Chin Huang <qaz6209031@gmail.com>
2021-05-30 02:27:14 -0700

    split the file structure

---
+++ data.py
@@ -0,0 +1,35 @@
+
+import requests
+import pandas as pd
+import sys
+from bs4 import BeautifulSoup
+
+
+DATA_SOURCE = 'https://bit.ly/3wGaAA0'
+
+def getData():
+   db = []
+   try:
+      page = requests.get(DATA_SOURCE)
+   except requests.exceptions.RequestException as err:
+      sys.exit('Connection error')
+
+   soup = BeautifulSoup(page.text, 'html.parser')
+   rows = soup.find('tbody').find_all('tr')
+
+   for row in rows:
+      datas = row.find_all('td')
+
+      date = datas[0].text
+      quantity = datas[2].text
+      route = datas[6].text.lower()
+      product = datas[7].text.lower()
+      customer = datas[8].text.lower()
+      dayref = datas[11].text.lower()
+
+      db.append([date, quantity, route, product, customer, dayref])
+
+   headers = ['Date', 'Quantity', 'Route', 'Product', 'Customer', 'Dayref']
+   df = pd.DataFrame(db, columns = headers)
+
+   return df
--- voice.py
+++ voice.py
@@ -1,12 +1,8 @@
-import aiy.voice.tts
-from aiy.board import Board
-from aiy.cloudspeech import CloudSpeechClient
-from bs4 import BeautifulSoup
-import pandas as pd
-import requests
-import sys
+# import aiy.voice.tts
+# from aiy.board import Board
+# from aiy.cloudspeech import CloudSpeechClient
 
-DATA_SOURCE = 'https://bit.ly/3wGaAA0'
+from data import getData
 def main():
     data = getData()
     print(data)
@@ -27,31 +23,6 @@ def main():
 #            elif 'goodbye' in text:
 #                break
 
-def getData():
-    db = []
-    try:
-        page = requests.get(DATA_SOURCE)
-    except requests.exceptions.RequestException as err:
-        sys.exit('Connection error')
 
-    soup = BeautifulSoup(page.text, 'html.parser')
-    rows = soup.find('tbody').find_all('tr')
-
-    for row in rows:
-        datas = row.find_all('td')
-
-        date = datas[0].text
-        quantity = datas[2].text
-        route = datas[6].text.lower()
-        product = datas[7].text.lower()
-        customer = datas[8].text.lower()
-        dayref = datas[11].text.lower()
-
-        db.append([date, quantity, route, product, customer, dayref])
-
-    headers = ['Date', 'Quantity', 'Route', 'Product', 'Customer', 'Dayref']
-    df = pd.DataFrame(db, columns = headers)
-    
-    return df
 if __name__ == '__main__':
     main()
