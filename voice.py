48bd901a9162d528a92d14c76d8171c56b90cbe3
Kai-Chin Huang <qaz6209031@gmail.com>
2021-05-30 21:23:27 -0700

    Add similarity logic and clean up code

--- voice.py
+++ voice.py
@@ -3,17 +3,18 @@ from aiy.board import Board
 from aiy.cloudspeech import CloudSpeechClient
 from nltk.stem import PorterStemmer
 from data import getDataAndHint
+from difflib import SequenceMatcher
+
+print('Retrieving the data')
+DATA, HINTS, CUSTOMERS, PRODUCTS = getDataAndHint()
+print('Data loaded')
 
 def main():
-    print('Retrieving the data')
-    data, hints, customers, products = getDataAndHint()
-    print('Data loaded')
-
     client = CloudSpeechClient()
     with Board() as board:
         while True:
             print('Say something or repeat after me or bye')
-            text = client.recognize(hint_phrases=hints)
+            text = client.recognize(hint_phrases = HINTS)
             if text is None:
                 print('You said nothing.')
                 continue
@@ -23,16 +24,16 @@ def main():
             query = normalizedQuery(query)
             print('Query is', query)
             print('Genrating response...')
-            response = classifyQuery(query, data, customers, products)
+            response = classifyQuery(query)
             print('Response', response)
             aiy.voice.tts.say(response)
 
 # Sample question: what is scout order for tomorrow?
 # Sample answer: scout gets 0 country batard 15 mini croissant 8 ham and cheese croissant 6 chocolate croissant 21 morning bun tomorrow
-def customerOrderTime(df, customer, time):
+def customerOrderTime(customer, time):
     # Get the result based on customer and time
-    filter = (df['Dayref'] == time) & (df['Customer'] == customer)
-    table = df.loc[filter]
+    filter = (DATA['Dayref'] == time) & (DATA['Customer'] == customer)
+    table = DATA.loc[filter]
 
     # Construct reponse
     response = ''
@@ -47,10 +48,10 @@ def customerOrderTime(df, customer, time):
 
 # Sample question: "What are morning bun orders for tomorrow"
 # Sample Answer: "Tomorrow bun orders are Scout 30 Linnaea's 4 Kreuzberg 10 Kraken 12 Coastal Peaks 6"
-def makeOrderTime(df, product, time):
+def makeOrderTime(product, time):
     # Get the result based on customer and time
-    filter = (df['Dayref'] == time) & (df['Product'] == product)
-    table = df.loc[filter]
+    filter = (DATA['Dayref'] == time) & (DATA['Product'] == product)
+    table = DATA.loc[filter]
 
     response = ''
     for ind in table.index:
@@ -64,9 +65,9 @@ def makeOrderTime(df, product, time):
 
 # Sample question: "Who gets 10 plain croissants today"
 # Sample Answer: "Sally Loos gets 10 croissants today and Kreuzberg gets 10 croissants today"
-def whoGetOrderTime(df, quantity, product, time):
-    filter = (df['Dayref'] == time) & (df['Product'] == product) & (df['Quantity'] == quantity)
-    table = df.loc[filter]
+def whoGetOrderTime(quantity, product, time):
+    filter = (DATA['Dayref'] == time) & (DATA['Product'] == product) & (DATA['Quantity'] == quantity)
+    table = DATA.loc[filter]
     
     response = ''
     for ind in table.index:
@@ -80,9 +81,9 @@ def whoGetOrderTime(df, quantity, product, time):
 
 #Sample question:  "How many baguettes does Novo get today."
 #Sample answer:  "Novo gets 10 baguettes today"
-def getQuantityOrderTime(df, product, customer, time):
-    filter = (df['Dayref'] == time) & (df['Product'] == product) & (df['Customer'] == customer)
-    table = df.loc[filter]
+def getQuantityOrderTime(product, customer, time):
+    filter = (DATA['Dayref'] == time) & (DATA['Product'] == product) & (DATA['Customer'] == customer)
+    table = DATA.loc[filter]
 
     # Assume the table only returns one row
     if table.empty:
@@ -100,23 +101,39 @@ def normalizedQuery(query):
     query = ' '.join(stemmedQuery)
     return query
 
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+def ngrams(inp, n):
+    inp = inp.split(' ')
+    output = []
+    for i in range(len(inp)-n+1):
+        output.append(inp[i:i+n])
+    return output
+
+def containSimilarSubstring(query ,items):
+    # Check if product exist
+    for i in items:
+        # find the length of p
+        length = len(i.split())
+        ngramList = [' '.join(x) for x in ngrams(query, length)]
+        for ngram in ngramList:
+            if similar(ngram, i) > 0.85:
+                return i
+    return ''
+
+
 # Decides which function does the query maps to
-def classifyQuery(query, data, customers, products):
+def classifyQuery(query):
     customer, time, product, quantity = '', '', '', ''
     response = ''
     times = ['today', 'tomorrow']
 
-    # Check if customer exist
-    for c in customers:
-        if c in query:
-            customer = c
-            break
-    
-    # Check if product exist
-    for p in products:
-        if p in query:
-            product = p
-            break
+    # Check if query contains string that similar to one of our product
+    customer = containSimilarSubstring(query, CUSTOMERS)
+
+    # Check if query contains string that similar to one of our product
+    product = containSimilarSubstring(query, PRODUCTS)
 
     # Check if time exist
     for t in times:
@@ -133,16 +150,16 @@ def classifyQuery(query, data, customers, products):
 
     if customer and time and product:
         print('Call getQuantityOrderTime')
-        response = getQuantityOrderTime(data, product, customer, time)
+        response = getQuantityOrderTime(product, customer, time)
     elif time and product and quantity:
         print('Call whoGetOrderTime')
-        response = whoGetOrderTime(data, quantity, product, time)
+        response = whoGetOrderTime(quantity, product, time)
     elif time and product:
         print('Call makeOrderTime')
-        response = makeOrderTime(data, product, time)
+        response = makeOrderTime(product, time)
     elif customer and time:
         print('Call customerOrderTime')
-        response = customerOrderTime(data, customer, time)
+        response = customerOrderTime(customer, time)
     else:
         response = "Sorry I don't recognize this question, please ask another one"
 
